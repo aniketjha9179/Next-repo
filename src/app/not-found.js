33b1e0485b32927250b6854725e74b16725388b7
Aniket Jha <84675061+aniketjha9179@users.noreply.github.com>
2023-06-28 17:00:20 +0530

    Delete src/app directory

--- src/app/not-found.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function notFound() {
-  return (
-    <h1>This page is not available </h1>
-  )
-}
-
-export default notFound
--- src/app/study/page.js
+++
@@ -1,7 +0,0 @@
-export default function study (){
-    return(
-        <div>
-            <h1>All Lectures of College</h1>
-        </div>
-    )
-}
