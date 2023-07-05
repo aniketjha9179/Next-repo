d108b29096a3fc0f776359cdb9a7a4043c406464
Aniket Jha <84675061+aniketjha9179@users.noreply.github.com>
2023-07-05 19:07:09 +0530

    userdetails/page.js
    asking for geolocation of the user

--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,16 @@
-"use client";
-import React from "react";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
+'use client'
+import Script from "next/script";
 
-function page() {
-  const router = useRouter();
-  const navigate =(name)=>{
-    router.push(name)
-  }
-
-  return (
-    <div>
-      <h1>basic page routing </h1>
-      <Link href={"./login"}>Go to Login Page</Link>
-      <br />
-      <Link href={"./about"}>Go to about Page</Link>
-      <br />
-      <br />
-      {/* <button onClick={() => { router.push("./login");}}> Go to login page</button> */}
-      <button onClick={() => { navigate("./login");}}> Go to login page</button>
-      <br />
-      <br />
-
-      <button onClick={() => { navigate("./about");}}> Go to  About</button>
-
-        
-    </div>
-  );
+export default function page(){
+    return(
+        <div>
+        <h1>Get user geolocatioon</h1>
+        <Script 
+        src="/location.js"
+        onLoad={()=>{
+            console.log('file loaded');
+        }}
+        />
+        </div>
+    )
 }
-
-export default page;
