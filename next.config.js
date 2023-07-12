72ce0e403b2871abd6c4f9db6b60df01a3521620
Aniket Jha <84675061+aniketjha9179@users.noreply.github.com>
2023-07-12 17:21:15 +0530

    //redirection using config file of components
    rediretion in from config file which is more convenient as compared to navigation

--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+//redirection using config file of components 
+const nextConfig = {
+    redirects:async()=>{
+        return [
+            {
+                source:'/about',
+                destination:'/',
+                permanent:false   
+
+            },
+            {
+                source:'/admin',
+                destination:'/',
+                permanent:false   
+
+            },
+            
+        ]
+    }
+};
+
+
+module.exports = nextConfig;
