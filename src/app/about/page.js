37878bc443d67f50bd7d286f6419a472db4191e4
Aniket Jha <84675061+aniketjha9179@users.noreply.github.com>
2023-07-12 17:19:04 +0530

    Update page.js
    redirection in web pages using navigation

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,30 +1,13 @@
-'use client'
-import Link from 'next/link';
 import React from 'react'
-import { useRouter } from "next/navigation";
+// import  {redirect} from 'next/navigation'
+//redirection using navigation
 
+const page = () => {
+    // redirect('/')
 
-const  about = ()=> {
-  const router= useRouter();
   return (
-    <div>
-        <h1>
-            hello about page 
-            {/* <Link href="/">go to home</Link> */}
-            <br />
-            <button onClick={() => { router.push("/");}}> Go to Home page</button>
-            <br />
-            <br />
-
-            <button onClick={()=>{router.push('/about/aboutstudent')}} >to to about stduent</button>
-            <br />
-            <button onClick={()=>{router.push('/about/abouteacher')}} >Go to about teacher</button>
-
-
-
-        </h1>
-    </div>
+    <div> ABout page</div>
   )
 }
 
-export default about;
+export default page
